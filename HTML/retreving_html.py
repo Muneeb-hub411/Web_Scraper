e3f3316b892b51cf0d564dcdb9a96f158556efdd
Muneeb-hub411 <muneebsaleem402@gmail.com>
2026-03-31 10:25:51 +0500

    -tried to scrape flipkart

--- HTML/retreving_html.py
+++ HTML/retreving_html.py
@@ -1,8 +1,8 @@
 import requests
 
-url="https://www.daraz.pk/mobile-cases-covers/?from=hp_categories&q=Mobile+Accessories&service=all_channel"
+url="https://www.flipkart.com/search?q=mobiles&as=on&as-show=on&otracker=AS_Query_TrendingAutoSuggest_1_0_na_na_na&otracker1=AS_Query_TrendingAutoSuggest_1_0_na_na_na&as-pos=1&as-type=TRENDING&suggestionId=mobiles&requestId=e00600e7-ac13-402c-bd72-1ea1a2d6a806"
 
 response=requests.get(url)
 
-with open("response.html", "w", encoding="utf-8") as file:
+with open("mobiles.html", "w", encoding="utf-8") as file:
     file.write(response.text)
